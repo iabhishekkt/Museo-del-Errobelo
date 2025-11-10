ba61102250dbb158cd8bb8a152455e2ebcc4033a
iabhishekkt <abhishekkumar.akt03@gmail.com>
2025-11-10 15:44:48 +0530

    build file corrected for deployment

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,26 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'components': [
+            './src/components/Boutique.jsx',
+            './src/components/Collection.jsx',
+            './src/components/Display.jsx',
+            './src/components/FAQ.jsx',
+            './src/components/FeaturedArtifacts.jsx',
+            './src/components/LandingPage.jsx',
+            './src/components/LifeAtMuseum.jsx',
+            './src/components/Palace.jsx',
+            './src/components/ThreeDGallery.jsx',
+            './src/components/Ticketing.jsx'
+          ]
+        }
+      }
+    }
+  }
 })
